909cc40a37b2adaa2540a16270548ca5e0c0810b
Fawaz KOURDOGHLI <91321838+fawazzo@users.noreply.github.com>
2026-01-03 23:58:50 +0300

    Change server port from 5000 to 10000

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use('/api/orders', orderRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(
   PORT,
